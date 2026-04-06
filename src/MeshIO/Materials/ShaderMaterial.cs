33324de894ab159dcf2a81488eb9ff3ea6a7098c
DomCR <domenech93@gmail.com>
2026-04-06 10:05:24 +0200

    docs

--- src/MeshIO/Materials/ShaderMaterial.cs
+++ src/MeshIO/Materials/ShaderMaterial.cs
@@ -1,5 +1,8 @@
 ﻿namespace MeshIO.Materials;
 
+/// <summary>
+/// Represents a material that uses a custom shader for rendering effects.
+/// </summary>
 public class ShaderMaterial : Material
 {
 	public ShaderMaterial() : this(string.Empty)
